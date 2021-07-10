c59326e0aa31cb75133a5a343335d5f52d9e6c03
jeswanthreddy1999 <49138515+jeswanthreddy1999@users.noreply.github.com>
2021-07-10 14:03:29 +0530

    Add files via upload

---
+++ selectors/script.js
@@ -0,0 +1,5 @@
+$("p").css({"color":"orange"});
+$("p").eq(1).css({"font-size":"0.5em"});
+$("#icecreamfloats li").css({"background-color": "orange"});
+$(".flavours li").css({"background-color":"lightblue", "font-size":"1.2em"});
+
